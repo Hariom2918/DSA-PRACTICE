edbcbc81eac0a8d8abf19b82b26402f772321960
Hari Om Gupta <hariom2917@gmail.com>
2026-07-28 09:48:59 +0530

    Time: 33 ms (65.26%) | Memory: 102.5 MB (16.47%) - LeetSync

---
+++ 155-min-stack/min-stack.java
@@ -0,0 +1,31 @@
+import java.util.Stack;
+
+class MinStack {
+
+    private Stack<int[]> stack;
+
+    public MinStack() {
+        stack = new Stack<>();
+    }
+
+    public void push(int val) {
+        if (stack.isEmpty()) {
+            stack.push(new int[]{val, val});
+        } else {
+            int minVal = Math.min(val, stack.peek()[1]);
+            stack.push(new int[]{val, minVal});
+        }
+    }
+
+    public void pop() {
+        stack.pop();
+    }
+
+    public int top() {
+        return stack.peek()[0];
+    }
+
+    public int getMin() {
+        return stack.peek()[1];
+    }
+}
