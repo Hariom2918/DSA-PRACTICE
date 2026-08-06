86c493cafa5b93e3528e505e5482a1d8633f73ee
Hari Om Gupta <hariom2917@gmail.com>
2026-08-06 23:48:00 +0530

    Time: 2 ms (97.84%) | Memory: 48.3 MB (52.53%) - LeetSync

--- 621-task-scheduler/task-scheduler.java
+++ 621-task-scheduler/task-scheduler.java
@@ -1,41 +1,22 @@
-class Task implements Comparable<Task>{
-    int freq;
-    int et;
-    Task(int f,int t){
-        freq = f;
-        et = t;
-    }
-    public int compareTo(Task that){
-        return that.freq - this.freq;
-    }
-}
 class Solution {
     public int leastInterval(char[] tasks, int n) {
-        HashMap<Character,Integer> fm = new HashMap<>();
+        
+        int[] freq = new int[26];
+
         for(char ch : tasks){
-            fm.put(ch,fm.getOrDefault(ch,0)+1);
+            freq[ch - 'A']++;
         }
-        PriorityQueue<Task> pq = new PriorityQueue<>();
-        for(Character ch: fm.keySet()){
-            int fre = fm.get(ch);
-            pq.offer(new Task(fre,0));
+        int maxFreq = 0;
+        for(int i : freq){
+            maxFreq = Math.max(maxFreq,i);
         }
-        Queue<Task> q = new LinkedList<>();
-        int time =0;
-        while(!q.isEmpty() || !pq.isEmpty()){
-            time++;
-            if(!pq.isEmpty()){
-                Task task = pq.poll();
-                task.freq--;
-                if(task.freq>0){
-                    task.et = time + n;
-                    q.offer(task);
-                }
-            }
-            if(!q.isEmpty() && q.peek().et == time){
-                pq.offer(q.poll());
-            }
+        int countMax = 0;
+        for(int i : freq){
+            if(i == maxFreq) countMax++;
         }
-        return time;
+
+        int part = (maxFreq - 1) * (n+1) + countMax;
+
+        return Math.max(tasks.length, part);
     }
 }
