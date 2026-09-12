f6e006f03b4ff3a6b3dcff1a1fc0999eddcc8243
Hari Om Gupta <hariom2917@gmail.com>
2026-09-12 12:23:07 +0530

    Time: 2 ms (38.91%) | Memory: 47.1 MB (76.07%) - LeetSync

---
+++ 801-is-graph-bipartite/is-graph-bipartite.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean isBipartite(int[][] graph) {
+        int n = graph.length;
+        int[] color = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            if (color[i] != 0) continue;
+
+            Queue<Integer> q = new LinkedList<>();
+            q.add(i);
+            color[i] = 1;
+
+            while (!q.isEmpty()) {
+                int node = q.poll();
+
+                for (int nei : graph[node]) {
+                    if (color[nei] == 0) {
+                        color[nei] = -color[node];
+                        q.add(nei);
+                    } 
+                    else if (color[nei] == color[node]) {
+                        return false;
+                    }
+                }
+            }
+        }
+
+        return true;
+    }
+}
